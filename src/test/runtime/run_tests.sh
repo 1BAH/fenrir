023218be31a7b6c80c15d7460b75a12953d677f8
Iван <koefic.cien@gmail.com>
2026-01-31 12:47:44 +0300

    fix PATH in test env

--- src/test/runtime/run_tests.sh
+++ src/test/runtime/run_tests.sh
@@ -1,14 +1,17 @@
 #! /usr/bin/env bash
 
+set -e
+
 FENRIR_TEST_DIR="/tmp/fenrir_test"
 FENRIR_TEST_DEBUG=${FENRIR_TEST_DEBUG:-"-v"}
 
 export FENRIR_TEST_DIR
 export FENRIR_TEST_DEBUG
 
-if ! ./setup; then
-    exit 1
-fi
+./setup
+
+PATH=$(cat "${FENRIR_TEST_DIR}/PATH_NEW")
+export PATH
 
 ./runner
 ./teardown
